05ffe9ccec91aec90430f7bb2936186582331cac
chetanshelke18 <chetanshelke18@gmail.com>
2023-09-22 17:54:23 +0530

    saveProduct

--- src/main/java/com/jbk/controller/ProductController.java
+++ src/main/java/com/jbk/controller/ProductController.java
@@ -5,14 +5,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jbk.model.Product;
+import com.jbk.service.ProductService;
+import com.jbk.serviceIMPL.ProductServiceIMPL;
 
 @RestController
 public class ProductController {
 	
+	ProductService service=new ProductServiceIMPL();
 	@PostMapping("/save-product")
 	public String saveProduct(@RequestBody Product product) {
-		System.out.println(product);
-		return "ok";
+		
+    String msg = service.saveProduct(product);		
+		return msg;
 		
 	}
 
